339046eac243a9b6500435395490dadcf89aa251
Stefano Fiorentino <stefano.fiore84@gmail.com>
2020-04-08 00:14:38 +0200

    08 - type alias [vector]
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -3,11 +3,23 @@
 
 template<class T> using vector = std::vector<T>; // introduced in C++11 (modern??)
 
+using int_vector = vector<int>; // introduced in C++11 (modern??)
+
 int main() {
+    {
         vector<int> values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         for(auto const &v : values) {
             std::cout << v;
         }
         std::cout << std::endl;
+    }
+    {
+        int_vector values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for(auto const &v : values) {
+            std::cout << v;
+        }
+        std::cout << std::endl;
+    }
+
     return 0;
 }
