5da1c0bbcdaae7b9fe8a239f8da05a6b08e994b8
Stefano Fiorentino <stefano.fiore84@gmail.com>
2020-04-08 00:11:03 +0200

    07 - alias template [vector]
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,98 +1,13 @@
 #include <iostream>
-#include <string>
+#include <vector>
 
-struct base {
-    base() {
-        std::cout << "base ctor" << std::endl;
-    }
-
-    virtual ~base();
-
-    virtual void hello() const {
-        std::cout << "Hello, I'm base" << std::endl;
-    }
-};
-
-base::~base() {
-    std::cout << "base d'ctor" << std::endl;
-}
-
-struct derived : public base {
-    derived() {
-        std::cout << "derived ctor" << std::endl;
-    }
-
-    ~derived() override {
-        std::cout << "derived d'ctor" << std::endl;
-    }
-
-    void hello() const override {
-        std::cout << "Hello, I'm derived" << std::endl;
-    }
-};
-
-namespace {
-    void hello(base const &b) {
-        b.hello();
-    }
-}
+template<class T> using vector = std::vector<T>; // introduced in C++11 (modern??)
 
 int main() {
-    {
-        base b; // creo un istanza dell'oggetto base chiamata b
-    } // all'uscita dallo scope, la distruggo
-    {
-        derived d; // crea un istanza dell'oggetto derived chiamando i due costruttori
-    } // all'uscita dallo scope, le istrugge in ordine inverso
-    {
-        base b;
-        b.hello();
-        derived d;
-        d.hello();
-    }
-    {
-        base b;
-        ::hello(b);
-        derived d;
-        ::hello(d);
-    }
-    {
-        using string = std::string;
-
-        string s;
-
-        s = "I'm a string";
-        std::cout << s << std::endl;
-
-        s += ".";
-        std::cout << s << std::endl;
-        std::cout << s.capacity() << std::endl;
-        std::cout << s.size() << std::endl;
-        std::cout << std::endl;
-
-        s.shrink_to_fit();
-        std::cout << s.capacity() << std::endl;
-        std::cout << std::endl;
-
-        s += s;
-        std::cout << s.capacity() << std::endl;
-        std::cout << s.size() << std::endl;
-        std::cout << std::endl;
-
-        s.shrink_to_fit();  // introduced in C++11 (modern??)
-        std::cout << s.capacity() << std::endl;
-        std::cout << s.size() << std::endl;
-        std::cout << std::endl;
-
-        string a = "I'm another string"; // string creation
-        a += "."; // string manipulation
-        for(auto it = 0u; it < 15u; it++) {
-            a += a; // appending allthings
+        vector<int> values = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        for(auto const &v : values) {
+            std::cout << v;
         }
-        std::cout << a.capacity() << std::endl; // capacity is twice the size (!!)
-        std::cout << a.size() << std::endl;
         std::cout << std::endl;
-    }
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
