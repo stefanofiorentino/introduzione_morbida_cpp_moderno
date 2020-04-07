a12d3df40fc59f5113f7dbbe3fd37ba4945a9dee
Stefano Fiorentino <stefano.fiore84@gmail.com>
2020-04-07 22:44:50 +0200

    03 - inheritance
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -5,9 +5,20 @@ struct base {
         std::cout << "base ctor" << std::endl;
     }
 
-    ~base() {
+    virtual ~base();
+};
 
-        std::cout << "base d'ctor" << std::endl;
+base::~base() {
+    std::cout << "base d'ctor" << std::endl;
+}
+
+struct derived : public base {
+    derived() {
+        std::cout << "derived ctor" << std::endl;
+    }
+
+    ~derived() override {
+        std::cout << "derived d'ctor" << std::endl;
     }
 };
 
@@ -15,6 +26,9 @@ int main() {
     {
         base b; // creo un istanza dell'oggetto base chiamata b
     } // all'uscita dallo scope, la distruggo
+    {
+        derived d; // crea un istanza dell'oggetto derived chiamando i due costruttori
+    } // all'uscita dallo scope, le istrugge in ordine inverso
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
