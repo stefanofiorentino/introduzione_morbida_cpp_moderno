047ee4a159dbe162e5e7fb2158b145584f317f5e
Stefano Fiorentino <stefano.fiore84@gmail.com>
2020-04-07 22:34:41 +0200

    02 - scope
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
 
+struct base {
+    base() {
+        std::cout << "base ctor" << std::endl;
+    }
+
+    ~base() {
+
+        std::cout << "base d'ctor" << std::endl;
+    }
+};
+
 int main() {
+    {
+        base b; // creo un istanza dell'oggetto base chiamata b
+    } // all'uscita dallo scope, la distruggo
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
