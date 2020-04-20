596848d6c6d5217c8cbcdbf0558f9739cfa96bdb
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2020-04-20 23:24:48 +0200

    14 - unique pointer for legacy code
    Signed-off-by: Stefano Fiorentino <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -9,41 +9,25 @@ struct base {
         std::cout << "base ctor" << std::endl;
     }
 
-    virtual ~base();
-
-    virtual void hello() const {
+    void hello() const {
         std::cout << "Hello, I'm base" << std::endl;
     }
 };
 
-base::~base() {
-    std::cout << "base d'ctor" << std::endl;
-}
-
-struct derived : public base {
-    derived() {
-        std::cout << "derived ctor" << std::endl;
-    }
-
-    ~derived() override {
-        std::cout << "derived d'ctor" << std::endl;
-    }
-
-    void hello() const override {
-        std::cout << "Hello, I'm derived" << std::endl;
-    }
-};
-
 namespace {
-    void hello(std::unique_ptr<base> bp) {
+    void hello(base *bp) { // legacy code
         bp->hello();
-    } // qui avviene la distruzione
+        delete bp; // this is missing in 80% of code, so it leaks
+    }
 }
 
 int main() {
     {
-        std::unique_ptr<derived> dp = std::make_unique<derived>();
-        hello(std::move(dp));
+        base *legacy_bp = new base; // ** PLEASE ** don't try this at work
+        hello(legacy_bp);
+
+        std::unique_ptr<base> bup = std::make_unique<base>();
+        hello(bup.release()); // only for legacy code: release the ownership
     }
     return 0;
 }
