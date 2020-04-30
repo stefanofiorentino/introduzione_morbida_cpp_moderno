c0ac8403b76e0722c704e54e46a8ad7b50115bae
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2020-04-30 23:56:58 +0200

    23 - lambda functions [parameters]
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 
 template<typename F, typename... Args>
 void hello(F &&f, Args... args) {
+    std::puts(__PRETTY_FUNCTION__);
     f(std::forward<Args>(args)...);
 }
 
 int main() {
-    hello([]() {
-        std::cout << "Hello" << std::endl;
-    });
+    hello([](std::string const &name) {
+        std::cout << "Hello " << name << std::endl;
+    }, "folks!");
     return 0;
 }
