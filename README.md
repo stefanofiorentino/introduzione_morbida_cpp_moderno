42dac9641fb72707a4ccbd9e42d97858e87f7974
Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>
2020-04-30 23:53:17 +0200

    22 - lambda functions
    Signed-off-by: Fiorentino Ing. Stefano <stefano.fiore84@gmail.com>

--- README.md
+++ README.md
@@ -4,3 +4,14 @@
 Introduzione morbida al C++ moderno.
 
 Repository legato alla trasmissione live streaming su https://www.twitch.tv/fiorentinoing/
+
+To start a new example: 
+```cpp
+cat << EOF > main.cpp
+#include <iostream>
+
+int main() {
+    return 0;
+}
+EOF
+```
--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 
-// Arthur O'Dwyer
-
-template<typename T, typename... Us>
-void f(Us... us) {
-    std::puts(__PRETTY_FUNCTION__);
+template<typename F, typename... Args>
+void hello(F &&f, Args... args) {
+    f(std::forward<Args>(args)...);
 }
 
 int main() {
-    f<char>(0, 1, 2);
-    f<char, char>(0, 1, 2);
-    f<char, char>(0, 1, 2, 4, 5, 6, 7);
+    hello([]() {
+        std::cout << "Hello" << std::endl;
+    });
     return 0;
 }
